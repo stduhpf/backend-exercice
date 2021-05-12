35ff56e33e63ddcf94626a63bdb3f478eaf90f8d
stduhpf <stephduh@live.fr>
2021-05-12 19:56:34 +0200

    question 3

---
+++ q3.py
@@ -0,0 +1,11 @@
+import psycopg2 as pc2
+
+connexion = pc2.connect("dbname=buildrz user=postgres password=stdRoot")
+
+cursor = connexion.cursor()
+
+cursor.execute("SELECT parcelle.addresse, parcelle.surface, projet.date_creation, projet.chiffre_affaire FROM projet INNER JOIN parcelle ON parcelle.parcelle_id = projet.parcelle_id WHERE parcelle.ville = 'Montreuil';")
+
+res = cursor.fetchall()
+
+print(res)

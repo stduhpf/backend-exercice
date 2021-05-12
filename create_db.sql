818f85a7191e98208d7f659b6783ed9e66ade972
stduhpf <stephduh@live.fr>
2021-05-12 17:18:25 +0200

    Initial commit
     Changes to be committed:
    	new file:   create_db.sql

---
+++ create_db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS user (
+    id      integer     CONSTRAINT pkuser PRIMARY KEY,
+    name    varchar(40) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS projet(
+    id              integer     CONSTRAINT pkproject PRIMARY KEY,
+    date_creation   date,
+    chiffre-affaire integer,
+    statut          varchar(10) CONSTRAINT statchk CHECK(statut = 'en cours' OR statut = 'terminé' OR statut = 'abandonné')
+);
+
+CREATE TABLE IF NOT EXISTS parcelle(
+    id          integer     CONSTRAINT pkparcelle PRIMARY KEY,
+    addresse    varchar(50),
+    ville       varchar(50),
+    codepostal  integer,
+    surface     integer 
+);git c

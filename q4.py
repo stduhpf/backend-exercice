9cdb9c890d26cafa275128bb8be6a518511a9aec
stduhpf <stephduh@live.fr>
2021-05-12 20:33:58 +0200

    template

--- q4.py
+++ q4.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 import psycopg2 as pc2
+
+connexion = pc2.connect("dbname=buildrz user=postgres password=stdRoot")
+cursor = connexion.cursor()
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def main():
+    cursor.execute("SELECT parcelle.addresse, parcelle.surface, projet.date_creation, projet.chiffre_affaire FROM projet INNER JOIN parcelle ON parcelle.parcelle_id = projet.parcelle_id WHERE parcelle.ville = 'Montreuil';")
+    res = cursor.fetchall()
+    return render_template("template.html")
--- runServ.ps1
+++ runServ.ps1
@@ -1,2 +1,3 @@
 $env:FLASK_APP="q4"
+$env:FLASK_ENV="development"
 flask run
